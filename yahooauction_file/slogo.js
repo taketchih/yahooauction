4737fc247fbacd7c73b5778ed10dbacc6f45511a
taketchih <162784474+taketchih@users.noreply.github.com>
2026-05-21 16:42:39 +0900

    Add files via upload

---
+++ yahooauction_file/slogo.js
@@ -0,0 +1,7 @@
+YAHOO.JP.smh.serviceLogo = [
+	{ srcUrl : 'shopping.yahoo.co.jp', andWebUrl : '' },
+	{ srcUrl : 'news.yahoo.co.jp', andWebUrl : '' },
+	{ srcUrl : 'transit.yahoo.co.jp', andWebUrl : '' },
+	{ srcUrl : 'roadway.yahoo.co.jp', andWebUrl : 'http://roadway.yahoo.co.jp/?deeplink=0' },
+	{ srcUrl : 'finance.yahoo.co.jp', andWebUrl : '' }
+];
